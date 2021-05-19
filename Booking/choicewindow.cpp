b5416de45c6924564d381cdc4cad110fd259cfa7
xenia marchenko <ksumar4enko@gmail.com>
2021-05-19 16:25:44 +0300

    update()

--- Booking/choicewindow.cpp
+++ Booking/choicewindow.cpp
@@ -16,7 +16,20 @@ ChoiceWindow::ChoiceWindow(const int idOfClient, QWidget *parent) :
     auto client = db::ClientAPI::getClientById(clientId);
 
     ui->label_2->setText(QString((client.fullName + "\nid: "+ std::to_string(client.id) + "\n" + client.phoneNumber+ "\n" +client.email).c_str()));
+    update();
 
+    connect(ui->pushButton, &QPushButton::clicked, [this](){
+        auto client = db::ClientAPI::getClientById(clientId);
+        auto order = db::ClientAPI::getOrderById(ui->comboBox->itemData(ui->comboBox->currentIndex()).toInt());
+        auto master = db::ClientAPI::getEmployeeById(order.employeeId);
+        db::ClientAPI::bookOrder(order.id, clientId);
+        QMessageBox::information(this, QString("Бронирование прошло успешно!"), QString((client.fullName + ", Вы забронировали: "+ order.title + " (с " + std::to_string(order.timeStart) + " до " + std::to_string(order.duration + order.timeStart) + "), мастер " + master.fullName).c_str()));
+        ui->comboBox->clear();
+        update();
+    });
+}
+
+void ChoiceWindow::update() {
     auto companies = db::ClientAPI::listCompanies();
     for (size_t i = 0; i < companies.size(); ++i) {
         auto orders = db::ClientAPI::listVacantOrdersOfCompany(companies[i]);
@@ -27,31 +40,10 @@ ChoiceWindow::ChoiceWindow(const int idOfClient, QWidget *parent) :
             ui->comboBox->addItem(QString(str.c_str()), QVariant(order.id));
         }
     }
-
-    connect(ui->pushButton, &QPushButton::clicked, [this](){
-        auto client = db::ClientAPI::getClientById(clientId);
-        auto order = db::ClientAPI::getOrderById(ui->comboBox->itemData(ui->comboBox->currentIndex()).toInt());
-        auto master = db::ClientAPI::getEmployeeById(order.employeeId);
-        db::ClientAPI::bookOrder(order.id, clientId);
-        QMessageBox::information(this, QString("Бронирование прошло успешно!"), QString((client.fullName + ", Вы забронировали: "+ order.title + " (с " + std::to_string(order.timeStart) + " до " + std::to_string(order.duration + order.timeStart) + "), мастер " + master.fullName).c_str()));
-        ui->comboBox->clear();
-        auto companies = db::ClientAPI::listCompanies();
-        for (size_t i = 0; i < companies.size(); ++i) {
-            auto orders = db::ClientAPI::listVacantOrdersOfCompany(companies[i]);
-            for (size_t k = 0; k < orders.size(); ++k) {
-                auto order = db::ClientAPI::getOrderById(orders[k]);
-                auto master = db::ClientAPI::getEmployeeById(order.employeeId);
-                std::string str = order.title + ", " + master.fullName + ", c " + std::to_string(order.timeStart) + " до " + std::to_string(order.duration + order.timeStart);
-                ui->comboBox->addItem(QString(str.c_str()), QVariant(order.id));
-            }
-        }
-    });
-
     auto bookedOrders = db::ClientAPI::listOrdersOfClient(clientId);
     QStandardItemModel *model = new QStandardItemModel;
     QStandardItem *item;
 
-    //Заголовки столбцов
     QStringList horizontalHeader;
     horizontalHeader.append("Событие");
     horizontalHeader.append("Мастер");
@@ -60,7 +52,7 @@ ChoiceWindow::ChoiceWindow(const int idOfClient, QWidget *parent) :
 
     model->setHorizontalHeaderLabels(horizontalHeader);
 
-    for (int i = 0; i < bookedOrders.size(); ++i) {
+    for (size_t i = 0; i < bookedOrders.size(); ++i) {
         auto order = db::ClientAPI::getOrderById(bookedOrders[i]);
         auto master = db::ClientAPI::getEmployeeById(order.employeeId);
         item = new QStandardItem(QString(order.title.c_str()));
@@ -76,13 +68,10 @@ ChoiceWindow::ChoiceWindow(const int idOfClient, QWidget *parent) :
         model->setItem(i, 3, item);
     }
     ui->tableView->setModel(model);
-
     ui->tableView->resizeRowsToContents();
     ui->tableView->resizeColumnsToContents();
     ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
-
-
 ChoiceWindow::~ChoiceWindow()
 {
     delete ui;
